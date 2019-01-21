3b828b2e79922ab39d21ddfa68c11225e89e9415
Majdi <majnurhasan@gmail.com>
2019-01-22 00:04:21 +0800

    Updated loginView.lua
    -- made texts and textfields for the login page and button for logging in

--- FriendFinder/loginView.lua
+++ FriendFinder/loginView.lua
@@ -5,36 +5,98 @@
 -----------------------------------------------------------------------------------------
 
 local composer = require( "composer" )
+local widget = require( "widget" )
 local scene = composer.newScene()
 
 function scene:create( event )
 	local sceneGroup = self.view
-	
+
 	local background = display.newRect( display.contentCenterX, display.contentCenterY, display.contentWidth, display.contentHeight )
 	background:setFillColor( 1 )	-- white
-	
+
+	-- functions below
+	local function textListener( event )
+		if ( event.phase == "began" ) then
+
+		elseif ( event.phase == "ended" or event.phase == "submitted" ) then
+			return event.target.text
+		elseif ( event.phase == "editing" ) then
+			return event.target.text
+		end
+	end
+
+	local function loginButtonEvent( event )
+ 
+		if ( event.phase == "ended" ) then
+			print( "Button was pressed and released" )
+		end
+	end
+
+
 	-- create some text
 	local title = display.newText( "FriendFinder", display.contentCenterX, 70, native.systemFont, 50 )
 	title:setFillColor( 0 )	-- black
 
-	local loginheader = display.newText("Login", display.contentCenterX, 200, native.systemFont, 20)
-	title:setFillColor ( 0 )
+	local loginheader = display.newText("Login", display.contentCenterX, 140, native.systemFont, 30)
+	loginheader:setFillColor ( 0 )
 	
-	local newTextParams = { text = "Loaded by the first tabs\n\"onPress\" listener\nspecified in the 'tabButtons' table", 
-						x = display.contentCenterX + 10, 
-						y = title.y + 215, 
-						width = 310, height = 310, 
-						font = native.systemFont, fontSize = 14, 
-						align = "center" }
-	local summary = display.newText( newTextParams )
-	summary:setFillColor( 0 ) -- black
+	local usernameParams = { text = "Username:", 
+						x = 80, 
+						y = loginheader.y + 70, 
+						width = 100, height = 50, 
+						font = native.systemFont, fontSize = 20, 
+						align = "left" }
+	local username = display.newText( usernameParams )
+	username:setFillColor( 0 )
+
+	local passwordParams = { text = "Password:", 
+						x = 80, 
+						y = loginheader.y + 110, 
+						width = 100, height = 50, 
+						font = native.systemFont, fontSize = 20, 
+						align = "left" }
+	local password = display.newText( passwordParams )
+	password:setFillColor( 0 )
+
+	local usernameTextField = native.newTextField( usernameParams.x + 140, loginheader.y + 55, 170, 35)
+	usernameTextField:setTextColor( 0 )
+	usernameTextField.isEditable = true
+	usernameTextField.size = 20
+	usernameTextField:addEventListener( "userInput", textListener )
+
+	local passwordTextField = native.newTextField( passwordParams.x + 140, loginheader.y + 95, 170, 35)
+	passwordTextField:setTextColor( 0 )
+	passwordTextField.isEditable = true
+	passwordTextField.size = 20
+	passwordTextField.isSecure = true	
+	passwordTextField:addEventListener( "userInput", textListener )
+
+	local loginButton = widget.newButton(
+		{
+			width = 40,
+			height = 40	,
+			defaultFile="button2.png",
+			overFile="button2-down.png",
+			label = "",
+			onEvent = loginButtonEvent,
+			x = display.contentCenterX,
+			y = 280
+		}
+	)
+	
+	
 
 	
 	-- all objects must be added to group (e.g. self.view)
 	sceneGroup:insert( background )
 	sceneGroup:insert( title )
-	sceneGroup:insert( loginheader)
-	sceneGroup:insert( summary )
+	sceneGroup:insert( loginheader )
+	sceneGroup:insert( username )
+	sceneGroup:insert( password )
+	sceneGroup:insert( usernameTextField )
+	sceneGroup:insert( passwordTextField )
+	sceneGroup:insert( loginButton )
+	
 end
 
 function scene:show( event )
