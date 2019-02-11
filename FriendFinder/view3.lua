b10e91fcb82130ed5808c6eacc7b24937fe91711
Majdi <majnurhasan@gmail.com>
2019-02-11 19:01:06 +0800

    Formatted view1.lua, view2.lua, view3.lua and view4.lua
    -- updated views for code readability

--- FriendFinder/view3.lua
+++ FriendFinder/view3.lua
@@ -10,6 +10,9 @@ local scene = composer.newScene()
 
 function scene:create( event )
 	local sceneGroup = self.view
+
+	local background = display.newRect( display.contentCenterX, display.contentCenterY, display.contentWidth, display.contentHeight )
+	background:setFillColor( 1 )	
 	
 	local function onFirstView( event )
 		composer.gotoScene( "view1" )
@@ -30,22 +33,17 @@ function scene:create( event )
 	local tabButtons = {
 		{ label="Finder", defaultFile="button1.png", overFile="button1-down.png", width = 32, height = 32, onPress=onFirstView },
 		{ label="Friends", defaultFile="button1.png", overFile="button1-down.png", width = 32, height = 32, onPress=onSecondView },
-		{ label="Groups", defaultFile="button1.png", overFile="button1-down.png", width = 32, height = 32, onPress=onThirdView, selected = true },
+		{ label="Groups", defaultFile="button1.png", overFile="button1-down.png", width = 32, height = 32, onPress=onThirdView },
 		{ label="Profile", defaultFile="button1.png", overFile="button1-down.png", width = 32, height = 32, onPress=onFourthView },
 	}
 	
 	local tabBar = widget.newTabBar{
-		top = display.contentHeight - 50,	-- 50 is default height for tabBar widget
+		top = display.contentHeight - 50,	
 		buttons = tabButtons
 	}
 	
-	-- create a white background to fill screen
-	local background = display.newRect( display.contentCenterX, display.contentCenterY, display.contentWidth, display.contentHeight )
-	background:setFillColor( 1 )	-- white
-	
-	-- create some text
 	local title = display.newText( "Groups Page", display.contentCenterX, 125, native.systemFont, 32 )
-	title:setFillColor( 0 )	-- black
+	title:setFillColor( 0 )	
 
 	local newTextParams = { text = "Loaded by the third tab's\n\"onPress\" listener\nspecified in the 'tabButtons' table", 
 							x = display.contentCenterX + 10, 
@@ -56,9 +54,9 @@ function scene:create( event )
 							fontSize = 14, 
 							align = "center" }
 	local summary = display.newText( newTextParams )
-	summary:setFillColor( 0 ) -- black
+	summary:setFillColor( 0 ) 
 	
-	-- all objects must be added to group (e.g. self.view)
+
 	sceneGroup:insert( background )
 	sceneGroup:insert( title )
 	sceneGroup:insert( summary )
@@ -72,12 +70,9 @@ function scene:show( event )
 	local phase = event.phase
 	
 	if phase == "will" then
-		-- Called when the scene is still off screen and is about to move on screen
+
 	elseif phase == "did" then
-		-- Called when the scene is now on screen
-		-- 
-		-- INSERT code here to make the scene come alive
-		-- e.g. start timers, begin animation, play audio, etc.
+
 	end	
 end
 
@@ -86,22 +81,15 @@ function scene:hide( event )
 	local phase = event.phase
 	
 	if event.phase == "will" then
-		-- Called when the scene is on screen and is about to move off screen
-		--
-		-- INSERT code here to pause the scene
-		-- e.g. stop timers, stop animation, unload sounds, etc.)
+
 	elseif phase == "did" then
-		-- Called when the scene is now off screen
+
 	end
 end
 
 function scene:destroy( event )
 	local sceneGroup = self.view
-	
-	-- Called prior to the removal of scene's "view" (sceneGroup)
-	-- 
-	-- INSERT code here to cleanup the scene
-	-- e.g. remove display objects, remove touch listeners, save state, etc.
+
 end
 
 ---------------------------------------------------------------------------------
